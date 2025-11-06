053d6993ad6e44cd3981f05c97778b1d7108d2a2
ManuJRS <manueljesusrejonsantana@gmail.com>
2025-11-06 11:22:14 -0600

    chore: new v-model exercise´s added vue practice 6-Nov-2025

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,12 @@ import BigImage from './components/BigImage.vue';
 import ClickMe from './components/ClickMe.vue';
 import EventsCounter from './components/EventsCounter.vue';
 import ImgHidden from './components/ImgHidden.vue';
+import InputModel from './components/InputModel.vue';
 import OperationCounter from './components/OperationCounter.vue';
 import VariablesReactivity from './components/VariablesReactivity.vue';
 
 </script>
 
 <template>
-<BigImage/>
+<InputModel/>
 </template>
---
+++ src/components/InputModel.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue';
+
+// Crea un <input> y un <p>.
+// Usa v-model para que al escribir en el input, el texto se muestre en el párrafo en tiempo real.
+let text = ref("Este texto va a cambiar")
+
+</script>
+
+<template>
+<h2>{{ text }}</h2>
+<input type="text" v-model="text">
+</template>
